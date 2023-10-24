f742814d052257d51528316368be9e7c3ed343fa
shrapple <145818219+shrapple@users.noreply.github.com>
2023-10-23 23:17:28 -0500

    ACTUAL FINAL
    FINAAAAALLLLY

--- python-challenge/python-challenge/PyBank/main.py
+++ python-challenge/python-challenge/PyBank/main.py
@@ -2,7 +2,7 @@ import os
 import csv
 csvpath_csv = os.path.join("Resources/budget_data.csv")
 
-#assigning variables values
+#assigning names and starting values to variables
 total = 0
 total_change = 0
 counter = 0    
@@ -11,12 +11,14 @@ greatest_decrease = 0
 greatest_increase_date = ""
 greatest_decrease_date = ""
 lastprofit_loss = 1088983
-#
+#opening and reading the csv
 with open(csvpath_csv) as csvfile:
     pybank_data = csv.reader(csvfile,delimiter =",")
     next(pybank_data, None)
-    
-
+#looking in each row of the data and creating new variables
+# and their values based on information found in each row 
+# hint: this is where you want to start and then create
+# and set your initial variables 
     for row in pybank_data:
         profit_loss = int(row[1])
         total = total + profit_loss
@@ -25,16 +27,20 @@ with open(csvpath_csv) as csvfile:
         dates = row[0]
         total_change = total_change + change
         counter = counter + 1
-
+#when it's checking each row, the change between the current and
+#next row is being checked against itself, and then moves on to
+# the next row to check for the demands created, here 
+# using greater or less than with the change between months
+#SHOUT OUT to tutor Matt Werth!
         if change > greatest_increase:
             greatest_increase = change
             greatest_increase_date = dates
         if change < greatest_decrease:
             greatest_decrease = change
             greatest_decrease_date = dates
-
+#an average equation for an average
 average = total_change/(counter-1)
-
+#print out our information concisely, but also nicely
 print("Financial Analysis")
 print("-------------------")
 print("Total Months:    " + str(counter))
@@ -42,8 +48,12 @@ print("Total:           $" + str(total))
 print("Average Change:  $" + "(%.2f)"%average)
 print("Greatest Increase in Profits:    " + str(greatest_increase_date) + "    ($" + str(greatest_increase) + ")")
 print("Greatest Decrease in Profits:    " + str(greatest_decrease_date) + "    ($" + str(greatest_decrease) + ")")
-
-
+#writes a text file for what is included after f.write.
+#the "a" means that if this python page is ran multiple times, it'll
+#just keep adding to the same txt file. "w" can be used when wanting to replacde
+f = open("myfile.txt","a") 
+f.write("Financial Analysis-    Total Months: 86,   Total: $22564198,   Average Change: $(-8311.11),    Greatest Increase in Profits: Aug-16 ($1862002),    Greatest Decrease in Profits: Feb-14 ($-1825558)")
+f.close()
 
 
 
--- python-challenge/python-challenge/PyPoll/main.py
+++ python-challenge/python-challenge/PyPoll/main.py
@@ -1,30 +1,31 @@
 import os
 import csv
 csvpath_csv = os.path.join("Resources/election_data.csv")
-
+#defining and setting initial values for my variables
 counter = 0
 candidates={"Charles Casper Stockham" : 0,
             "Diana DeGette" : 0,
             "Raymon Anthony Doane" : 0}
-
+#opening and reading the csv
 with open(csvpath_csv) as csvfile:
     election_data = csv.reader(csvfile,delimiter =",")
     next(election_data, None)
-     
+#checking row by row (for loop)
     for row in election_data:
         votes = row[2]
         counter = counter + 1
+#checking each entry in the 3 column for candidates entered names
         if votes == "Charles Casper Stockham":
             candidates["Charles Casper Stockham"] = candidates["Charles Casper Stockham"] + 1
         if votes == "Diana DeGette":
             candidates["Diana DeGette"] = candidates["Diana DeGette"] + 1
         if votes == "Raymon Anthony Doane":
             candidates["Raymon Anthony Doane"] = candidates["Raymon Anthony Doane"] + 1
-
+#finding and labeling some percent variables
 ccs_percent = (candidates["Charles Casper Stockham"]/counter)*100
 dd_percent = (candidates["Diana DeGette"]/counter)*100
 rad_percent = (candidates["Raymon Anthony Doane"]/counter)*100   
-
+#displaying it nicely for the consumer
 print("Election Results")
 print("-------------------")
 print("Total Votes:    " + str(counter))
@@ -35,3 +36,7 @@ print("Raymon Anthony Doane: " + "%.3f"%rad_percent + "% (" + str(candidates["Ra
 print("-------------------")
 print("Winner:  Diana DeGette")
 print("-------------------")
+#writing a text file containting the string ("Election Results, Total Votes:....")
+f = open("myfile.txt","a") 
+f.write("Election Results-  Total Votes: 369711,    Charles Casper Stockham: 23.049% (85213),   Diana DeGette: 73.812% (272892),    Raymon Anthony Doane: 3.139% (11606),   Winner: Diana DeGette")
+f.close()
